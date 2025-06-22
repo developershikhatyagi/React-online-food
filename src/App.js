06d3c2ddfbfe158121863d63dcd156661da9474c
Shikha <shikhatyagi419@catalyseresearch.com>
2025-06-22 19:23:35 +0530

    create-component,pages shikha

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './Pages/Cart';
+import Products from './Pages/Products';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navigation/>
+       <Routes>
+        <Route path='/' exact element={<Home/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/products' element={<Products/>}></Route>
+       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
